725fa0e94a9efc58e531ad038dded146d9ceb04b
Safirl <loic.leforestier@mmibordeaux.com>
2026-07-06 09:25:46 +0200

    Added three to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import restart from "vite-plugin-restart";
+import glsl from "vite-plugin-glsl";
+import { fileURLToPath } from "url";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "/src/assets/styles/_variables.scss" as *;
+          `,
+        },
+      },
+    },
+    plugins: [
+      restart({ restart: ["../public/**"] }), // Restart server on static file change
+      glsl(), // Handle shader files
+    ],
+    resolve: {
+      alias: {
+        "@plugins": fileURLToPath(new URL("./src/plugins", import.meta.url)),
+        "@shaders": fileURLToPath(new URL("./src/shaders", import.meta.url)),
+      },
+    },
+  },
+});
---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const projects = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    video: z.string(),
+    images: z.array(z.string()),
+    published: z.boolean(),
+  }),
+});
+
+export const collections = {
+  projects: projects,
+};
